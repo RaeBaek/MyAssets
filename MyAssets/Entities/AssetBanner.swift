78cbeb805752925a7df30ce51527dd356bd72985
RaeHoon Baek <bmp117@naver.com>
2022-10-06 17:00:11 +0900

    MyAssets 예제 - 배너 만들기

---
+++ MyAssets/Entities/AssetBanner.swift
@@ -0,0 +1,14 @@
+//
+//  AssetBanner.swift
+//  MyAssets
+//
+//  Created by 백래훈 on 2022/10/06.
+//
+
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
